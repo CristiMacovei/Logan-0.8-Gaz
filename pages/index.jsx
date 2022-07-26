5a7f87d5146f407cb8f902f29a662139240bb1ef
BotezatuHoria <horia12btz@gmail.com>
2022-07-26 16:25:16 +0300

    tailwindcss starter

---
+++ pages/index.jsx
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+    <div className='flex flex-row items-center justify-center'>
+      <span className='underline text-2xl cursor-pointer'>Hello from tailwind</span>
+    </div>
+  )
+}
